c58ff83420e6e5776abf58b0c625c10f95a655e8
dikehprosper <103826657+dikehprosper@users.noreply.github.com>
2023-01-23 21:37:46 -0500

    ninth commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basepath:"/prediction-app",
+  assetPrefix:"/prediction-app"
 }
 
 module.exports = nextConfig
